66574cba32c986d3260705401a59a53bb6e7f15e
sagargg <info@sagarg.com.np>
2021-05-18 16:31:07 +0545

    [group][s]: featured groups sorted with dataset number

--- ckanext/iaea/plugin.py
+++ ckanext/iaea/plugin.py
@@ -1,13 +1,33 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import ckan.logic as logic
+ 
 
+def featured_group():
+    try:
+        group_list = logic.get_action('group_list')(
+            {}, {'sort': 'package_count', 'all_fields': True, 'limit': 6})
+        if group_list: 
+            return group_list
+        else:
+            return logic.get_action('group_list')(
+                {}, {'all_fields': True, 'limit': 6})
+    except (logic.NotFound, logic.ValidationError, logic.NotAuthorized):
+        return {}
 
+    
 class IaeaPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers, inherit=True)
 
     # IConfigurer
-
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'iaea')
+
+    def get_helpers(self):
+        return {
+            'featured_group': featured_group
+        }
+        
