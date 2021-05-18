aefd1b569bfdd09467a1920d91cd578a1a9588f6
sagargg <info@sagarg.com.np>
2021-05-18 17:03:20 +0545

    [improve][xs] removing limit

--- ckanext/iaea/plugin.py
+++ ckanext/iaea/plugin.py
@@ -6,12 +6,12 @@ import ckan.logic as logic
 def featured_group():
     try:
         group_list = logic.get_action('group_list')(
-            {}, {'sort': 'package_count', 'all_fields': True, 'limit': 6})
+            {}, {'sort': 'package_count', 'all_fields': True})
         if group_list: 
             return group_list
         else:
             return logic.get_action('group_list')(
-                {}, {'all_fields': True, 'limit': 6})
+                {}, {'all_fields': True})
     except (logic.NotFound, logic.ValidationError, logic.NotAuthorized):
         return {}
 
