4fff08e0ddf639305821056674376033049cd3a9
blagoja <blagojabozinovski@gmail.com>
2024-02-23 15:31:35 +0100

    Custom translation config

--- ckanext/iaea/plugin.py
+++ ckanext/iaea/plugin.py
@@ -1,14 +1,15 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.lib.plugins import DefaultTranslation
 
 
-class IaeaPlugin(plugins.SingletonPlugin):
+class IaeaPlugin(plugins.SingletonPlugin, DefaultTranslation):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.ITranslation)
 
     # IConfigurer
 
     def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('assets', 'iaea')
+        toolkit.add_template_directory(config_, "templates")
+        toolkit.add_public_directory(config_, "public")
+        toolkit.add_resource("assets", "iaea")
